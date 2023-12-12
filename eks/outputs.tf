a56eaf41120dc5905998be0f2961b526c573849e
Apinya <apinyarr@hotmail.com>
2023-12-11 21:11:50 -0500

    add eks and correct other configs

---
+++ eks/outputs.tf
@@ -0,0 +1,15 @@
+output "cluster_name" {
+  value = module.doct_cbd3324_eks.cluster_name
+}
+
+output "cluster_identity_oidc_issuer" {
+  value = module.doct_cbd3324_eks.oidc_provider
+}
+
+output "cluster_identity_oidc_issuer_arn" {
+  value = module.doct_cbd3324_eks.oidc_provider_arn
+}
+
+output "cluster_id" {
+  value = module.doct_cbd3324_eks.cluster_id  
+}
